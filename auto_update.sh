e9e5e5a17964ea7272563674a99699a34377b19d
methylamphetamine <cngryphus@gmail.com>
2023-11-10 01:30:11 -0500

    new l2 all runs

---
+++ auto_update.sh
@@ -0,0 +1,4 @@
+sleep 6h
+git add --all
+git commit -m "new l2 all runs"
+git push
---
+++ run_new_error.sh
@@ -0,0 +1,6 @@
+python TrainCNO_rel_l2.py wave_0_5
+python TrainCNO_rel_l2.py allen
+python TrainCNO_rel_l2.py cont_tran
+python TrainCNO_rel_l2.py disc_tran
+python TrainCNO_rel_l2.py airfoil
+python TrainCNO_rel_l2.py darcy
